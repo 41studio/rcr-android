3231c25c7bf72ae4498dfe5a64615edd55298fca
ririsrismawati <riris.risma@yahoo.co.id>
2017-01-10 15:33:31 +0700

    fixed #7

--- app/src/main/java/com/fourtyonestudio/rcr/ui/activity/EditAreaActivity.java
+++ app/src/main/java/com/fourtyonestudio/rcr/ui/activity/EditAreaActivity.java
@@ -18,7 +18,6 @@ import com.fourtyonestudio.rcr.networks.RestApi;
 import com.fourtyonestudio.rcr.preferences.DataPreferences;
 import com.fourtyonestudio.rcr.utils.CommonUtils;
 import com.fourtyonestudio.rcr.utils.KeyboardUtils;
-import com.fourtyonestudio.rcr.utils.Retrofit2Utils;
 import com.fourtyonestudio.rcr.utils.UIHelper;
 
 import org.json.JSONObject;
@@ -61,6 +60,17 @@ public class EditAreaActivity extends AppCompatActivity {
         }
     }
 
+    private void attemptClone() {
+
+        if (TextUtils.isEmpty(etNameArea.getText().toString())) {
+            UIHelper.showSnackbar(getCurrentFocus(), "Please input name area");
+            etNameArea.requestFocus();
+
+        } else {
+            cloneArea();
+        }
+    }
+
     @OnClick(R.id.btnDelete)
     public void delete(View view) {
         KeyboardUtils.hideSoftKeyboard(this, view);
@@ -99,6 +109,48 @@ public class EditAreaActivity extends AppCompatActivity {
         KeyboardUtils.hideSoftKeyboard(this, view);
         attemptAdd();
 
+    }
+
+    @OnClick(R.id.btnClone)
+    public void clone(View view) {
+        KeyboardUtils.hideSoftKeyboard(this, view);
+        attemptClone();
+
+    }
+
+    private void cloneArea() {
+        new DataPreferences(this).setLoadArea(true);
+        if (CommonUtils.isNetworkAvailable(this)) {
+            final ProgressDialog pDialog = UIHelper.showProgressDialog(this);
+            DataPreferences dataPreferences = new DataPreferences(this);
+            LoginSession loginSession = dataPreferences.getLoginSession();
+            new RestApi().getApi().cloneArea(loginSession.getAuthToken(), id, etNameArea.getText().toString()).enqueue(new Callback<AreaItemResponse>() {
+                @Override
+                public void onResponse(Call<AreaItemResponse> call, Response<AreaItemResponse> response) {
+                    UIHelper.dismissDialog(pDialog);
+                    if (response.isSuccessful()) {
+
+                        finish();
+
+                    } else {
+                        try {
+                            JSONObject jObjError = new JSONObject(response.errorBody().string());
+                            UIHelper.showSnackbar(getCurrentFocus(), jObjError.getString(Constant.MESSAGE.ERROR_BODY));
+                        } catch (Exception e) {
+                            UIHelper.showSnackbar(getCurrentFocus(), e.getMessage());
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<AreaItemResponse> call, Throwable t) {
+                    UIHelper.dismissDialog(pDialog);
+                    UIHelper.showSnackbar(getCurrentFocus(), Constant.MESSAGE.ERROR_POST);
+                }
+            });
+        } else {
+            UIHelper.showSnackbar(getCurrentFocus(), Constant.MESSAGE.NO_INET);
+        }
     }
 
     private void addArea() {
